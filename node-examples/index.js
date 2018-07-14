358a50e71b12431921613e6136888fff59e44192
Rishabh Paul <rishabhpaul77@gmail.com>
2018-07-15 01:09:15 +0530

    Understanding node modules

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,8 +1,4 @@
-var rect = {
-    perimeter: (x ,y) => (2 * (x + y)),
-    area: (x ,y) => (x * y)
-};
-
+var rect = require('./rectangle');
 function solveRectangle(l ,b) {
     console.log("Solving for rectangle with l = " +l+ " and b = "+b);
 
@@ -13,7 +9,7 @@ function solveRectangle(l ,b) {
    }
 }
 
-solveRectangle(2 ,4);
+solveRectangle(1 ,4);
 solveRectangle(3 ,5);
 solveRectangle(-1 ,0);
 solveRectangle(0 ,0);
---
+++ node-examples/rectangle.js
@@ -0,0 +1,4 @@
+exports.perimeter = (x ,y) => (2 * (x + y));    //Using Arrow functions
+exports.area = function(x ,y) {
+    return x * y;
+}
