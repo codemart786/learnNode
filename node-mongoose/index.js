1c4a0dd023a649fc864753d2a9e2af3163ee55ad
Rishabh Paul <rishabhpaul77@gmail.com>
2018-07-16 03:05:12 +0530

    understanding schemas and sub-schemas

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -2,33 +2,44 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
-const url ='mongodb://localhost:27017/conFusion';
+const url = 'mongodb://localhost:27017/conFusion';
 
 const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
-    var newDish = Dishes({
-        name : "Rishabh786786" ,
-        description : "test mongoose"
-    });
-    newDish.save()
-    .then((dish) => {
-        console.log(dish);
-        return Dishes.find({}).exec();
+    Dishes.create({
+        name: "Rishabh786786",
+        description: "test mongoose"
     })
-    .then((dishes) => {
-        console.log(dishes);
-        return newDish.collection.drop();   //to drop a collection in mongoose
-    })
-    .then(() => {
-        return mongoose.connection.close(); //to close a database connection in mongoose 
-    })
-    .then(() => {
-        console.log('DB connection successfully closed');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .then((dish) => {
+            console.log(dish);
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: { description: 'Updated Test' }
+            }, {
+                    new: true
+                }).exec();
+        })
+        .then((dish) => {
+            console.log(dish);
+            dish.comments.push({
+                rating:5 ,
+                comment:'T T T',
+                author:'Rishabh'
+            });
+            return dish.save();
+        }).then((dish) => {
+            console.log(dish);
+            return Dishes.collection.drop();   //to drop a collection in mongoose
+        })
+        .then(() => {
+            return mongoose.connection.close(); //to close a database connection in mongoose 
+        })
+        .then(() => {
+            console.log('DB connection successfully closed');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }).catch((err) => {
-    console.log(err); 
+    console.log(err);
 });
