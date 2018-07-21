a0a2a01a622a57f60d865eba5528daa7b4591bb8
Rishabh Paul <rishabhpaul77@gmail.com>
2018-07-22 03:10:49 +0530

    Adding user signup and login

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -40,48 +40,31 @@ app.use(session({name : 'session-id' ,
 }));
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  console.log(req.signedCookies);
-  console.log(req.session)
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
       var err = new Error('You are not authenticated !!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next(err);
-    }
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username === 'admin' && password === 'password') {
-      //res.cookie('user' ,username ,{signed : true});
-      req.session.user = username;
-      next();
-    } else {
-      var err = new Error('The username and password is not correct');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-  } else {
-    if(req.session.user === 'admin') {
+    } 
+  else {
+    if(req.session.user === 'authenticated') {
         next();
     } else {
       var err = new Error('You are not authenticated');
       res.setHeader('WWW-Authenticate' ,'Basic');
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
 }
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 
 // catch 404 and forward to error handler
---
+++ conFusionServer/models/user.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username  :{
+        type : String , 
+        unique : true,
+        required : true
+    } , 
+    password : {
+        type : String , 
+        required : true
+    },
+    admin : {
+        type: Boolean , 
+        default : false
+    }
+});
+module.exports = mongoose.model('User' ,UserScehma);
